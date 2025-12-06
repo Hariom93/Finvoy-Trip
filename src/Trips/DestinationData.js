488f37d7bb0a1eb579459db74adab74ae4f5820a
Hariom Choudhary <165458145+Hariom93@users.noreply.github.com>
2025-12-06 16:27:02 +0530

    added new files

---
+++ src/Trips/DestinationData.js
@@ -0,0 +1,69 @@
+const DestinationData = [
+  {
+    id: 1,
+    name: "Hong Kong",
+    title: "Explore Hongkong",
+    tagline: "Uncover The Magic That Never Sleeps",
+    price: 21999,
+    duration: "6N/7D",
+    image: "https://images.unsplash.com/photo-1523482580672-f109ba8cb9be?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+
+    category: "International",
+    description:
+      "Hong Kong is a vibrant city known for its skyline, nightlife, shopping, and cultural blend."
+  },
+
+  {
+    id: 2,
+    name: "Bali",
+    title: "Bali",
+    tagline: "Iconic beaches and temple trials",
+    price: 8900,
+    duration: "4N/5D",
+          image: "https://images.unsplash.com/photo-1493976040374-85c8e12f0c0e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+    category: "International",
+    description:
+      "Bali is famous for its stunning beaches, peaceful temples, and tropical beauty."
+  },
+
+  {
+    id: 3,
+    name: "Maldives",
+    title: "Maldives",
+    tagline: "The Island of Tropical Bliss",
+    price: 48000,
+    duration: "5N/6D",
+          image: "https://images.unsplash.com/photo-1523482580672-f109ba8cb9be?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+    category: "International",
+    description:
+      "The Maldives is a luxury island destination known for clear water, water villas, and romantic vibes."
+  },
+
+  {
+    id: 4,
+    name: "Thailand",
+    title: "Thailand",
+    tagline: "Land of smiles and shores",
+    price: 10999,
+    duration: "5N/6D",
+         image: "https://images.unsplash.com/photo-1539603114212-ca6c3d76fd4f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+    category: "International",
+    description:
+      "Thailand is popular for beaches, nightlife, food, temples, and friendly culture."
+  },
+
+  {
+    id: 5,
+    name: "Vietnam",
+    title: "Vietnam",
+    tagline: "Pho fun and lots more",
+    price: 8999,
+    duration: "4N/5D",
+         image: "https://images.unsplash.com/photo-1525625293386-3f8f99389edd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+    category: "International",
+    description:
+      "Vietnam offers a beautiful mix of nature, street food, culture, and affordable travel."
+  }
+];
+
+export default DestinationData;
