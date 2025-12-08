f10b49af048fd33326c87cd6e4400995423f760e
satyam-malgaya <satyam.malgaya@gmail.com>
2025-12-08 18:56:47 +0530

    adding details page

--- src/routes/Allroutes.jsx
+++ src/routes/Allroutes.jsx
@@ -1,55 +1,58 @@
+import HolidaysDetails from "../pages/HolidaysDetails"; 
 import Navbar from '../components/Navbar';
 import React from 'react';
 import MainHome from '../pages/MainHome';
-import MyTrips from "../pages/account/MyTrips"; // ✅ matches folder structure
-import FinvoyTripWallet from "../pages/account/FinvoyTripWallet"; // ✅ matches folder structure
+import MyTrips from "../pages/account/MyTrips";
+import FinvoyTripWallet from "../pages/account/FinvoyTripWallet";
 import Offers from "../pages/Offers";
 import PackagePage from "../pages/PackagePage";
-import Account from "../pages/account/Account"; // ✅ matches folder structure
-import InviteCard from "../pages/account/InviteCard"; // ✅ matches folder structure
+import Account from "../pages/account/Account";
+import InviteCard from "../pages/account/InviteCard";
 import Work from "../pages/Work";
 import HongKongPage from "../pages/HongKongPage";
 import SaudiPage from "../pages/SaudiPage";
 import WesternAmericaPage from "../pages/WesternAmericaPage";
-import BusPass from "../pages/account/Buspas"; // ✅ folder structure corrected
+import BusPass from "../pages/account/Buspas";
 import { Routes, Route } from "react-router-dom";
 import HolidayDetails from '../Trips/HolidayDetails';
 import Flightdetail from '../Flight/FlightDetails';
 import ScrollToTop from '../components/ScrollToTop';
-import SavePayment from '../pages/account/SavePayment'
+import SavePayment from '../pages/account/SavePayment';
 import FlightResults from '../Flight/flights';
 import Review from '../Flight/Flightcan';
 import SeatSelection from '../Flight/SeatSelect';
-import Holidays from '../pages/Holidays'
-import HolidaysDetails from '../pages/HolidaysDetails'
+import Holidays from '../pages/Holidays';
+// import DestinationPage from '../pages/DestinationPage';  // ⭐ add this
+
 const Allroutes = () => {
   return (
     <>
-      <ScrollToTop /> 
+      <ScrollToTop />
       <div className="pb-16">
         <Routes>
-        <Route path="/HolidaysDetails/:id" element={<HolidaysDetails />} />
+          {/* ⭐ Dynamic destination route */}
+          {/* <Route path="/destination/:place" element={<DestinationPage />} /> */}
+          {/* Holidays routes */}
+          <Route path="/holidays" element={<Holidays />} />
+          <Route path="/destination/:id" element={<HolidaysDetails />} />
           <Route path="/" element={<MainHome />} />
           <Route path="/account/mytrips" element={<MyTrips />} />
           <Route path="/account/InviteCard" element={<InviteCard />} />
           <Route path="/account/finvoytripwallet" element={<FinvoyTripWallet />} />
-          <Route path="/account/SavePayment" element={<SavePayment />} />
+          <Route path="/account/savepayment" element={<SavePayment />} />
           <Route path="/account/buspass" element={<BusPass />} />
-          <Route path="/account" element={<Account />} /> {/* ✅ fixed path */}
+          <Route path="/account" element={<Account />} />
           <Route path="/offers" element={<Offers />} />
           <Route path="/work" element={<Work />} />
           <Route path="/flight" element={<Flightdetail />} />
           <Route path="/holiday" element={<HolidayDetails />} />
-          <Route path="/holidays" element={<Holidays />} />
           <Route path="/package/:id" element={<PackagePage />} />
           <Route path="/hongkong" element={<HongKongPage />} />
           <Route path="/saudi" element={<SaudiPage />} />
           <Route path="/western-america" element={<WesternAmericaPage />} />
-          <Route path="/results" element={<FlightResults/>} />
-          <Route path="/flightconf" element={<Review/>} />
-        <Route path="/seats" element={<SeatSelection />} />
-
-
+          <Route path="/results" element={<FlightResults />} />
+          <Route path="/flightconf" element={<Review />} />
+          <Route path="/seats" element={<SeatSelection />} />
         </Routes>
       </div>
       <Navbar />
