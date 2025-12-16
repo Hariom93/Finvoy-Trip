441135f34fbd5b1ad662324b1111b95bed85c70b
satyam-malgaya <satyammalgaya50@gmail.com>
2025-12-16 15:43:01 +0530

    Fix import case for BackButton component

--- src/Trips/TripComponents/DestinationDetail.jsx
+++ src/Trips/TripComponents/DestinationDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import BackButton from "../../components/backbutton";
+import BackButton from "../../components/Backbutton";
 
 import {
   ArrowLeft,
